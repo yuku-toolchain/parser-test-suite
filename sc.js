ccf62ea0466a80028368295aec6b71d1d5fdfc4b
arshad <arshadpyaseen@gmail.com>
2026-02-01 15:56:20 +0530

    update

--- sc.js
+++
@@ -1,8 +0,0 @@
-const glob = new Bun.Glob("pass/**/*");
-
-console.log(Array.from(glob.scanSync(".")).length)
-
-// for (let file of glob.scanSync(".")) {
-//   const contents = await Bun.file(file).text()
-//   await Bun.write(`js/pass/${file.replace("pass/", "")}`, contents)
-// }
