46344136eb00357a32fc67f4333438fbae5fbd36
arshad <arshadpyaseen@gmail.com>
2026-08-17 09:26:20 +0530

    revert(ts): drop the ts/fail bucket

--- ts/fail/d8644e21c6eb5c15.module.ts
+++
@@ -1,7 +0,0 @@
-import type Required = require("a");
-
-import Alias = Namespace.Member;
-import type TypeAlias = Namespace.Member;
-import type Shorthand = Local;
-
-export import Exported = Namespace.Member;
